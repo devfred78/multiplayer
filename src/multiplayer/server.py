c0e85d9cf637061a98ebaf5f69ebf165b0f2a69f
devfred78 <devfred78@gmail.com>
2026-05-17 17:56:00 +0200

    fix(authentication): require server_password for list_games if set

--- src/multiplayer/server.py
+++ src/multiplayer/server.py
@@ -289,7 +289,7 @@ def _handle_client(conn, addr, games, groups, lock, server_passwords, logger_nam
                 # Check if it's an admin action
                 is_server_admin_action = action in ['stop_server', 'restart_server', 'get_server_info', 'set_logging_for_server', 'set_logging_enabled', 'list_all_players', 'get_cert_expiration', 'set_server_password', 'set_admin_password', 'create_group', 'remove_group', 'set_persistent_players_enabled', 'kick_player', 'kick_observer']
                 is_group_admin_action = action in ['kick_player', 'kick_observer', 'set_group_admin_password']
-                is_public_action = action in ['list_groups', 'get_group_info', 'list_group_games', 'list_games', 'list_users']
+                is_public_action = action in ['list_groups', 'get_group_info', 'list_group_games', 'list_users']
                 is_persistent_player_action = action in ['create_persistent_player']
                 
                 # If it's a kick action, it could be server admin OR group admin
